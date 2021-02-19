d1338691375abe68d5a7ebb30818b5bbd406a25d
Aditya Pundir <68149659+Aditya-Pundir@users.noreply.github.com>
2021-02-19 09:34:01 -0800

    Add files via upload

---
+++ user_camera/script.js
@@ -0,0 +1,9 @@
+let canvas = document.querySelector("#canvas");
+let video = document.querySelector("#video");
+
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+  navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
+    video.srcObject = stream;
+    video.play();
+  });
+}
